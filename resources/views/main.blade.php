8bd2688b2897dbd89b0df58999957cbd0fd58028
MohammadMortezaie <mohammadmortezaie.s@gmail.com>
2023-10-06 14:23:16 -0700

    fix logo

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -3,9 +3,14 @@
 
 <head>
 
-    <link rel="icon" type="image/png"  href="{{ URL::to(env('APP_URL') . '/img/favicon.ico') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ URL::to(env('APP_URL') . '/img/apple-touch-icon.png') }}">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ URL::to(env('APP_URL') . '/img/favicon-32x32.png') }}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ URL::to(env('APP_URL') . '/img/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ URL::to(env('APP_URL') . '/img/site.webmanifest') }}">
+    <link rel="mask-icon" href="{{ URL::to(env('APP_URL') . '/img/safari-pinned-tab.svg') }}" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{ URL::to(env('APP_URL') . '/css/bootstrap.min.css') }}">
